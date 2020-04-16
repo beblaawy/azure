806abf132633036822363239cb55fdbcd0e8e854
Hesham AbdAllah <hesham.abdallah.ha@gmail.com>
2020-04-16 23:08:08 +0200

    create employees app

---
+++ precache-manifest.fbb9af57bb510639eca8277cc300ce51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861813a8de5dbe91749e9aa918e1a083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28918e9e9dc56b6d2a8",
+    "url": "/static/css/main.741c6b7a.chunk.css"
+  },
+  {
+    "revision": "c8981b8cd5461fa5ecd8",
+    "url": "/static/js/2.cd3eaf4f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cd3eaf4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28918e9e9dc56b6d2a8",
+    "url": "/static/js/main.dde85773.chunk.js"
+  },
+  {
+    "revision": "0b7dd1fec5054eda56c6",
+    "url": "/static/js/runtime-main.0ecec90c.js"
+  }
+]);
